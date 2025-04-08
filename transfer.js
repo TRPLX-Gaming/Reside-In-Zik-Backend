1db4a139f19387bd573929f53980488de8251f04
TRPLX_Gaming <icgabriel2007@gmail.com>
2025-04-08 22:07:53 +0100

    Add files via upload

--- transfer.js
+++ transfer.js
@@ -1,12 +1,18 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
-const dataSourcePath = 'data.txt'
-const data = fs.readFileSync(dataSourcePath)
+const dataSourcePath = 'data.json'
+const data = JSON.parse(fs.readFileSync(dataSourcePath))
+console.log(data)
 
 const server = http.createServer((req,res) => {
-    res.writeHead(200,{
-        'Content-Type':'text/plain'
-    })
-    res.end(data)
+    const parsedUrl = url.parse(req.url)
+    if(parsedUrl.pathname === '/data') {
+        res.writeHead(200,{
+           'Content-Type':'text/html'
+        })
+        res.end(`<p>${JSON.stringify(data)}</p>`)
+        console.log(JSON.stringify(data))
+    }
 }).listen(3000,()=>{console.log('hosted')})
